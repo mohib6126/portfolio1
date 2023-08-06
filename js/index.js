b0071207d3067563c2f7bac33643d9beea575ab8
Mohib ur rehman <118989366+mohib6126@users.noreply.github.com>
2023-08-06 20:47:35 +0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+ 
+   $(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        autoplay:true,
+        autoplayHoverPause:true,
+        loop: true,
+        margin:30,
+        autoplaySpeed:2000,
+        animateIn:true,
+        items:3,
+        responsiveClass:true
+    });
+  });
